87b49fa2509ef5e5cdddfbc4f2c9ac7f3ab68fbd
Trần Mạnh Trường <tmtruong2505@gmail.com>
2026-07-15 17:52:44 +0700

    lab 4

--- learn-typescript/src/lab4.ts
+++ learn-typescript/src/lab4.ts
@@ -74,6 +74,6 @@ function identity<T>(value: T): T {
     return value;
 }
 
-identity<number>(10);
-identity<string>("Truong");
-identity<boolean>(true);
+console.log(identity<number>(10));
+console.log(identity<string>("Truong"));
+console.log(identity<boolean>(true));
